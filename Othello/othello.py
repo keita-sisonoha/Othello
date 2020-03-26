3bd13fc35a07087a215abee76c99b351dee44e23
keita-sisonoha <tsuruyama822@gmail.com>
2020-03-26 16:24:08 +0900

    othello

--- Othello/othello.py
+++ Othello/othello.py
@@ -3,48 +3,48 @@ WHITE = "○"
 BLACK = "●"
 
 banmen = [
-[EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-[EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-[EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-[EMPTY,EMPTY,EMPTY,WHITE,BLACK,EMPTY,EMPTY,EMPTY],
-[EMPTY,EMPTY,EMPTY,BLACK,WHITE,EMPTY,EMPTY,EMPTY],
-[EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-[EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-[EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
-]
+    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
+    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
+    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
+    [EMPTY,EMPTY,EMPTY,WHITE,BLACK,EMPTY,EMPTY,EMPTY],
+    [EMPTY,EMPTY,EMPTY,BLACK,WHITE,EMPTY,EMPTY,EMPTY],
+    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
+    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
+    [EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY,EMPTY],
+]cd
 
 def show_banmen():
-for i in range(len(banmen) + 1):
-for j in range(len(banmen[0]) + 1):
-if i == 0:
-if j == 0:
-print(" ", end="")
-else:
-print(j - 1, end="")
-else:
-if j == 0:
-print(i - 1, end="")
-else:
-print(banmen[i - 1][j - 1], end="")
-print()
+    for i in range(len(banmen) + 1):
+        for j in range(len(banmen[0]) + 1):
+            if i == 0:
+                if j == 0:
+                    print(" ", end="")
+                else:
+                    print(j - 1, end="")
+            else:
+                if j == 0:
+                    print(i - 1, end="")
+                else:
+                    print(banmen[i - 1][j - 1], end="")
+        print()
 
 class ReversiBord(object):
 
     # 実際に盤面を変更（ひっくり返す）する処理
-    def flip_disk(self, x, y, color):
+    def fllip_disk(self, x, y, color):
+        fllippables = self.list_fllippable_disk(x, y, color)
 
-        flippables = self.list_flippable_disk(x, y, color)
-
-        if len(flippables) == 0:
+        if len(fllippables) == 0:
             return False
-
+        
         banmen[y][x] = color
-        for pos in flippables:
+        for pos in fllippables:
             banmen[pos[1]][pos[0]] = color
+
         return True
 
     # 配置を試してひっくり返す候補を取得する
-    def list_flippable_disk(self,x,y,color):
+    def list_fllippable_disk(self,x,y,color):
         direction = [
             (dir_x, dir_y)
             for dir_x in (-1, 0, 1)
@@ -53,13 +53,14 @@ class ReversiBord(object):
         ]
 
         # 8方向を石がおけるか検査する
-        flippables = []
+        fllippables = []
         for dir in direction:
-            flippables += self.list_flippable_disk_one_line(x, y, dir[0], dir[1], color)
+            fllippables += self.list_fllippable_disk_one_line(x, y, dir[0], dir[1], color)
+        
+        return fllippables
 
-        return flippables
+    def list_fllippable_disk_one_line(self, x, y, dir_x, dir_y, color):
 
-    def list_flippable_disk_one_line(self, x, y, dir_x, dir_y, color):
         reversed_color = BLACK if color == WHITE else WHITE
         reversed_list = []
         pos_x = x
@@ -81,27 +82,29 @@ class ReversiBord(object):
             # 自分の石がおかれていた場合は、検査を終了する。
             else:
                 break
-
+            
         return reversed_list
 
+
 def input_numeric(text):
-while True:
-ret = input(text)
-try:
-ret = int(ret)
-break
-except:
-print('[警告]：数値を入力してください')
+    while True:
+        ret = input(text)
+        try:
+            ret = int(ret)
+            break
+        except:
+            print('[警告]：数値を入力してください')
 
     return ret
 
-if **name** == '**main**':
-  
- # Player は 2 人固定のため、ターンの判定は 0/1 で判定を行う # ＜定義＞ 0 : 黒 、 1 : 白
-turn = 0
-board = ReversiBord()
 
-    # while True ?
+if __name__ == '__main__':
+    
+    # Plyaer は2人固定のため、ターンの判定は 0/1 で判定を行う
+    # ＜定義＞ 0 : 黒 、 1 : 白
+    turn = 0
+    board = ReversiBord()
+
     while True:
         print("{0}のターンです".format('黒' if turn == 0 else '白'))
         color = BLACK if turn == 0 else WHITE
@@ -116,8 +119,7 @@ board = ReversiBord()
             for x in range(len(banmen[y])):
                 if banmen[y][x] != EMPTY:
                     continue
-                    #
-                if 0 < len(board.list_flippable_disk(x, y, color)):
+                if 0 < len(board.list_fllippable_disk(x, y, color)):
                     skip = False
                 gameover = False
 
@@ -139,9 +141,9 @@ board = ReversiBord()
                 print('範囲内を入力してください')
             else:
                 break
-
-        if 0 < len(board.list_flippable_disk(pos_x, pos_y, color)):
-            board.flip_disk(pos_x, pos_y, color)
+        
+        if 0 < len(board.list_fllippable_disk(pos_x, pos_y, color)):
+            board.fllip_disk(pos_x, pos_y, color)
             turn = (turn + 1) % 2
         else:
             print('その場所はおけません')
